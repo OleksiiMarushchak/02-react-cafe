7a49bac7e905118212b68b2039d7791dc0a4e528
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2026-01-08 22:17:48 +0000

    fix name const

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
                 <VoteStats
                     votes={votes}
                     totalVotes={totalVotes}
-                    positivePercentage={positiveRate}
+                    positiveRate={positiveRate}
                 />
             ) : (
                 <Notification />
