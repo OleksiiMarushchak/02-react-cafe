b50b6760ff7c39f62a667e988c5c87281d3f912b
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2026-01-08 22:28:24 +0000

    gix interfaces 2

--- src/types/votes.ts
+++ src/types/votes.ts
@@ -6,19 +6,3 @@ export interface Votes {
 
 export type VoteType = "good" | "neutral" | "bad";
 
-
-export interface VoteStatsProps {
-    votes: {
-        good: number;
-        neutral: number;
-        bad: number;
-    };
-    totalVotes: number;
-    positiveRate: number;
-}
-
-export interface VoteOptionsProps {
-    onVote: (type: VoteType) => void;
-    onReset: () => void;
-    canReset: boolean;
-}
